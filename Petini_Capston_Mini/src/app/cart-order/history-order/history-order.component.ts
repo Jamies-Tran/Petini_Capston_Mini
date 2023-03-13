b6f8c6ae535d378f261ccffce3ddd164cd086ebd
quangnp1712 <87289670+quangnp1712@users.noreply.github.com>
2023-03-14 02:23:43 +0700

    history order done

--- Petini_Capston_Mini/src/app/cart-order/history-order/history-order.component.ts
+++ Petini_Capston_Mini/src/app/cart-order/history-order/history-order.component.ts
@@ -9,14 +9,51 @@ import { OrderService } from '../../services/order.service';
 })
 export class HistoryOrderComponent implements OnInit{
   message:any;
-  values:any;
+  values:any[]=[];
   constructor(private http:OrderService , private image: ImageService){  }
 
   ngOnInit(): void {
       try {
         let name = localStorage.getItem('username') as string;
-        this.http.getAllOrderOfCustomer(name).subscribe((data)=>{
+        this.http.getAllOrderOfCustomer(name).subscribe(async (data)=>{
           console.log(data);
+          let datas = data;
+          let carts: any[] = [];
+          let productName = '';
+          let productQuantity = '';
+          let totalPrice = '';
+          let createdBy = '';
+          let price = '';
+          let createdDate= '';
+          let imageUrl = '' ;
+          if (data) {
+            for (let data of datas) {
+              carts = [];
+              let cart = data.cart;
+              for (let cartProduct of cart.cartProduct) {
+                let product = cartProduct.product;
+                productName = product.name;
+                productQuantity = cartProduct.quantity;
+                price = product.price;
+                await this.image.getImage('items/' + product.imageUrl).then((url)=>{
+                  imageUrl = url;
+                })
+                carts.push({ product: productName, quantity: productQuantity, price:price, imageUrl:imageUrl});
+              }
+              totalPrice = cart.totalPrice;
+              createdBy = data.createdBy;
+              createdDate = data.createdDate;
+              this.values.push({
+                name: createdBy,
+                carts: carts,
+                total: totalPrice,
+                status: data.status,
+                id: data.id,
+                createdDate:createdDate
+              });
+            }
+          }
+          console.log(this.values);
         },(error)=>{
           console.log(error);
         })
