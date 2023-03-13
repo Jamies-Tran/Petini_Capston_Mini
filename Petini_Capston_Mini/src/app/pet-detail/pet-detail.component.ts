46f553da17c9079c1ccaeec626ede690f27f82b3
quangnp1712 <87289670+quangnp1712@users.noreply.github.com>
2023-03-13 22:55:47 +0700

    pet detail done

--- Petini_Capston_Mini/src/app/pet-detail/pet-detail.component.ts
+++ Petini_Capston_Mini/src/app/pet-detail/pet-detail.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PetService } from '../services/pet.service';
+import { ImageService } from '../services/image.service';
 
 @Component({
   selector: 'app-pet-detail',
   templateUrl: './pet-detail.component.html',
-  styleUrls: ['./pet-detail.component.scss']
+  styleUrls: ['./pet-detail.component.scss'],
 })
-export class PetDetailComponent {
+export class PetDetailComponent implements OnInit {
+  message: any;
+  name!: string;
+  values: any;
+  imageUrl: any;
 
+  constructor(private http: PetService, private image: ImageService) {}
+  ngOnInit(): void {
+    try {
+      let name = '';
+      name = localStorage.getItem('getPetName') as string;
+      this.http.getPetByName(name).subscribe(async (data) => {
+        console.log(data);
+        this.values = data;
+        await this.image.getImage('pets/' + data.imageUrl).then((url) => {
+          this.imageUrl = url;
+        });
+      });
+    } catch (error) {}
+  }
 }
--- Petini_Capston_Mini/src/app/pet/pet.component.ts
+++ Petini_Capston_Mini/src/app/pet/pet.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PetService } from '../services/pet.service';
+import { ImageService } from '../services/image.service';
 
 @Component({
   selector: 'app-pet',
   templateUrl: './pet.component.html',
   styleUrls: ['./pet.component.scss']
 })
-export class PetComponent {
+export class PetComponent implements OnInit{
+  values : any[]=[];
+  value:any;
 
+  public onItemSelector(name: string) {
+    localStorage.setItem('getPetName', name);
+  }
+
+ngOnInit(): void {
+  this.http.getPetListByStatus('NOT_ADOPTED').subscribe(async (data) => {
+    console.log(data);
+    for(this.value of data){
+
+          var imgUrl = await this.image.getImage('pets/' + this.value.imageUrl)
+          console.log("imaURL:" , imgUrl);
+          this.values.push({imgURL:imgUrl, name:this.value.name, age:this.value.age, weight:this.value.weight })
+          console.log("values:", this.values);
+
+
+
+      }
+      });
+}
+
+constructor(private http: PetService,private image: ImageService){}
 }
